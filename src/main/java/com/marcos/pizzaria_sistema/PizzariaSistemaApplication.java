a79a12781faff6ee07388161d57c8fb5222c0710
Marcos Santos <marcossantosmsms604@gmail.com>
2024-06-27 00:00:17 -0300

    Projeto criado

---
+++ src/main/java/com/marcos/pizzaria_sistema/PizzariaSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.pizzaria_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/pizzaria_sistema/PizzariaSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.pizzaria_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
