c0e4837712185ff2e7ec632fd36dd7795b26104b
marcossantos1 <marcossantosmsms604@gmail.com>
2024-06-30 20:06:55 -0300

    classe pedido

---
+++ src/main/java/com/marcos/pizzaria_sistema/model/Pedido.java
@@ -0,0 +1,10 @@
+package com.marcos.pizzaria_sistema.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+}
